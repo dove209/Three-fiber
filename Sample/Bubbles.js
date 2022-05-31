0fdfdf45daf42f26ffe79c882086889f8c0a750e
GINI <dove209@naver.com>
2022-05-31 12:16:53 +0900

    투명 버블 추가

--- Sample/Bubbles.js
+++ Sample/Bubbles.js
@@ -1,22 +1,31 @@
 import * as THREE from 'three';
-import React, { useRef, Suspense, useEffect } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { OrbitControls, ContactShadows, Instances, Instance } from '@react-three/drei'
-import { EffectComposer, SSAO } from '@react-three/postprocessing'
+import { useRef } from 'react';
+import { Canvas,  useFrame, useThree } from '@react-three/fiber';
+import { OrbitControls, Instances, Instance} from '@react-three/drei'
 
-const particles = new Array(150).fill().map(() => ({
+// 파란색 버블
+const particles = new Array(30).fill().map(() => ({
   factor: THREE.MathUtils.randInt(20, 100),
   speed: THREE.MathUtils.randFloat(0.01, 1),
-  xFactor: THREE.MathUtils.randFloatSpread(80),
-  yFactor: THREE.MathUtils.randFloatSpread(40),
-  zFactor: THREE.MathUtils.randFloatSpread(40)
+  xFactor: THREE.MathUtils.randFloatSpread(20),
+  yFactor: THREE.MathUtils.randFloatSpread(20),
+  zFactor: THREE.MathUtils.randFloatSpread(4)
+}))
+
+// 투명 버블
+const glass_bubbles = new Array(10).fill().map(() => ({
+  factor: THREE.MathUtils.randInt(20, 100),
+  speed: THREE.MathUtils.randFloat(0.01, 1),
+  xFactor: THREE.MathUtils.randFloatSpread(20),
+  yFactor: THREE.MathUtils.randFloatSpread(20),
+  zFactor: THREE.MathUtils.randFloatSpread(10)
 }))
 
 const Bubble = ({ factor, speed, xFactor, yFactor, zFactor }) => {
   const ref = useRef();
   useFrame((state) => {
     const t = factor + state.clock.elapsedTime * (speed / 2);
-    ref.current.scale.setScalar(Math.max(1.5, Math.cos(t) * 5));
+    ref.current.scale.setScalar(Math.max(1.5, Math.cos(t) * 2.5));
     ref.current.position.set(
       Math.cos(t) + Math.sin(t * 1) / 10 + xFactor + Math.cos((t / 10) * factor) + (Math.sin(t * 1) * factor) / 10,
       Math.sin(t) + Math.cos(t * 2) / 10 + yFactor + Math.sin((t / 10) * factor) + (Math.cos(t * 2) * factor) / 10,
@@ -24,22 +33,16 @@ const Bubble = ({ factor, speed, xFactor, yFactor, zFactor }) => {
     )
   })
   return (
-    <Instance ref={ref} />
+    <Instance position={[xFactor, yFactor, zFactor ]} ref={ref} />
   )
 }
 
 
 const Bubbles = () => {
-  const ref = useRef();
-  useFrame((state, delta) => {
-    ref.current.rotation.y = THREE.MathUtils.damp(ref.current.rotation.y, (-state.mouse.x * Math.PI) / 6, 2.8, delta);
-    ref.current.rotation.x = THREE.MathUtils.damp(ref.current.rotation.x, (-state.mouse.y * Math.PI) / 6, 2.8, delta);
-
-  })
-  return(
-    <Instances limit={particles.length} ref={ref} castShadow receiveShadow position={[0, 10, 0]}>
-      <sphereBufferGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial roughness={0} color="#f0f0f0" />
+  return (
+    <Instances limit={particles.length} position={[0, 0, -10]}>
+      <sphereBufferGeometry args={[THREE.MathUtils.randFloat(0.8, 1.5), 32, 32]} />
+      <meshStandardMaterial roughness={0.5} metalness={0} color={'blue'} />
       {particles.map((data, i) => (
         <Bubble key={i} {...data} />
       ))}
@@ -47,32 +50,60 @@ const Bubbles = () => {
   )
 }
 
+const Glass_Bubbles = () => {
+  return (
+    <Instances limit={glass_bubbles.length} position={[0, 0, -10]}>
+      <sphereBufferGeometry args={[THREE.MathUtils.randFloat(0.5, 0.9), 32, 32]} />
+      <meshPhysicalMaterial roughness={0} metalness={0} transmission={1} thickness={2} />
+      {glass_bubbles.map((data, i) => (
+        <Bubble key={i} {...data} />
+      ))}
+    </Instances>
+  )
+}
+
+
+
 function App() {
   return (
     <Canvas
-      shadows
-      gl={{ antialias: false }}
-      dpr={[1, 2]}
-      camera={{ position:[0,0,60],fov: 75, near: 10, far:150 }}
+      gl={{ antialias: true, toneMapping: THREE.ACESFilmicToneMapping, toneMappingExposure: 1.2  }}
+      dpr={[1,2]}
+      camera={{ position: [0, 0, 8], fov: 45, near: 1, far: 100 }}
     >
-      <color attach={'background'} args={['#f0f0f0']} />
-      <fog attach={'fog'} args={['#fff', 60, 110]} />
-      <ambientLight intensity={1.5} />
-      <pointLight position={[100, 10, -50]} intensity={20} castShadow />
-      <pointLight position={[-100, -100, -100]} intensity={10} color='red' />
-
+      <color attach={'background'} args={['#000']} />
+      <ambientLight intensity={1} />
+      <spotLight
+        intensity={3}
+        angle={0.2}
+        penumbra={1}
+        position={[100, 100, 100]}
+        castShadow
+        shadow-mapSize={[512, 512]}
+      />
+      <directionalLight intensity={3} position={[100, 100, 100]} color='purple' />
       <Bubbles />
-
-      <ContactShadows position={[0, -30, 0]} opacity={0.6} scale={130} blur={1} far={40} />
-      <EffectComposer multisampling={0}>
-        <SSAO samples={31} radius={0.1} intensity={30} luminanceInfluence={0.1} color={'red'} />
-      </EffectComposer>
-      {/* Helper ETC */}
-      <axesHelper args={[10]} />
-      <OrbitControls makeDefault />
+      <Glass_Bubbles />
+      <Mouse />
+      {/* <axesHelper args={[10]}/> */}
+      {/* <OrbitControls makedefault /> */}
     </Canvas>
   );
 }
 
-export default App;
+const Mouse = () => {
+  const ref = useRef();
+  const {viewport: { width, height }} = useThree();
+  useFrame((state) => {
+    ref.current.position.set(state.mouse.x * width / 2, state.mouse.y * height / 2, 0)
+  })
+  return(
+    <mesh ref={ref}>
+      <sphereBufferGeometry args={[2, 32, 32]} />
+      <meshPhysicalMaterial roughness={0} metalness={0} transmission={1} thickness={2} />
+    </mesh>
+  )
+}
 
+
+export default App;
