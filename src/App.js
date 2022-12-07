49d9722afcbdbc1638ea1eaeb7600c4267e49633
dove209 <53247748+dove209@users.noreply.github.com>
2022-12-07 23:20:52 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './pages/Header';
+import Header from "./pages/Header";
 
-
-import FlyingDoughnut from "./pages/Flying_Doughnut";
-import CameraPan from './pages/CameraPan';
-import Ballpit from './pages/Ballpit';
-import Confetti from './pages/Confetti';
-import DreiVideoTexture from './pages/DreiVideoTexture';
-import HorizontalTiles from './pages/HorizontalTiles';
-import HtmlAnnotation from './pages/HtmlAnnotation';
-import ImageGallery from './pages/ImageGallery';
-import InfiniteScroll from './pages/InfiniteScroll';
-import RedBallClump from './pages/RedBallClump';
-import SoftShadows from './pages/SoftShadows';
-import SpiralStair from './pages/SpiralStair';
-import TossInternship from './pages/TossInternship';
-import WobbleSphere from './pages/WobbleSphere';
-import EnvironmentBlur from "./pages/EnvironmentBlur";
-
+const FlyingDoughnut = lazy(() => import("./pages/Flying_Doughnut"));
+const CameraPan = lazy(() => import("./pages/CameraPan"));
+const Ballpit = lazy(() => import("./pages/Ballpit"));
+const Confetti = lazy(() => import("./pages/Confetti"));
+const DreiVideoTexture = lazy(() => import("./pages/DreiVideoTexture"));
+const HorizontalTiles = lazy(() => import("./pages/HorizontalTiles"));
+const HtmlAnnotation = lazy(() => import("./pages/HtmlAnnotation"));
+const ImageGallery = lazy(() => import("./pages/ImageGallery"));
+const InfiniteScroll = lazy(() => import("./pages/InfiniteScroll"));
+const RedBallClump = lazy(() => import("./pages/RedBallClump"));
+const SoftShadows = lazy(() => import("./pages/SoftShadows"));
+const SpiralStair = lazy(() => import("./pages/SpiralStair"));
+const TossInternship = lazy(() => import("./pages/TossInternship"));
+const WobbleSphere = lazy(() => import("./pages/WobbleSphere"));
+const EnvironmentBlur = lazy(() => import("./pages/EnvironmentBlur"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<FlyingDoughnut />} />
-        <Route path="/camera-pan" element={<CameraPan />} />
-        <Route path="/ball-pit" element={<Ballpit />} />
-        <Route path="/confetti" element={<Confetti />} />
-        <Route path="/Drei-videoTexture" element={<DreiVideoTexture />} />
-        <Route path="/horizontal-tiles" element={<HorizontalTiles />} />
-        <Route path="/html-annotation" element={<HtmlAnnotation />} />
-        <Route path="/image-gallery" element={<ImageGallery />} />
-        <Route path="/infinite-scroll" element={<InfiniteScroll />} />
-        <Route path="/red-ball-clump" element={<RedBallClump />} />
-        <Route path="/soft-shadows" element={<SoftShadows />} />
-        <Route path="/spiral-stair" element={<SpiralStair />} />
-        <Route path="/toss-internship" element={<TossInternship />} />
-        <Route path="/wobble-sphere" element={<WobbleSphere />} />
-        <Route path='/environment-blur-transition' element={<EnvironmentBlur />} />
-      </Routes>
+      <Suspense fallback={<div>...로딩중</div>}>
+        <Routes>
+          <Route path="/" element={<FlyingDoughnut />} />
+          <Route path="/camera-pan" element={<CameraPan />} />
+          <Route path="/ball-pit" element={<Ballpit />} />
+          <Route path="/confetti" element={<Confetti />} />
+          <Route path="/Drei-videoTexture" element={<DreiVideoTexture />} />
+          <Route path="/horizontal-tiles" element={<HorizontalTiles />} />
+          <Route path="/html-annotation" element={<HtmlAnnotation />} />
+          <Route path="/image-gallery" element={<ImageGallery />} />
+          <Route path="/infinite-scroll" element={<InfiniteScroll />} />
+          <Route path="/red-ball-clump" element={<RedBallClump />} />
+          <Route path="/soft-shadows" element={<SoftShadows />} />
+          <Route path="/spiral-stair" element={<SpiralStair />} />
+          <Route path="/toss-internship" element={<TossInternship />} />
+          <Route path="/wobble-sphere" element={<WobbleSphere />} />
+          <Route path="/environment-blur-transition" element={<EnvironmentBlur />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-
-  )
+  );
 };
 
 export default App;
